33cf11382870612f38636b59e4b75e0dbc2706be
jang-juyeong <rosetta0724@gmail.com>
2021-06-01 17:32:02 +0900

    backup sprints

---
+++ im-sprint-cli-practice-master/getListMultiplesOfTwo.js
@@ -0,0 +1,8 @@
+const { range } = require('range'); // range 모듈을 불러옵니다
+
+function getListMultiplesOfTwo(upTo) {
+range(0, upTo+1, 2)
+return range(2,upTo+1, 2)
+}
+
+module.exports = getListMultiplesOfTwo;
